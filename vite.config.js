94795ab7139930ebf97648eeb49a058752adaa3c
MemoTonui <tonuilinda@gmail.com>
2023-05-08 18:05:55 +0300

    Make Heroku Fix

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,15 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      output:{
+          manualChunks(id) {
+              if (id.includes('node_modules')) {
+                  return id.toString().split('node_modules/')[1].split('/')[0].toString();
+              }
+          }
+      }
+  }
   }
 })
